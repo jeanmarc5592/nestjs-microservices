37dc2cdf7ab9e5a5591bf06ac19f6ba32a1b4001
jeanmarcmoeckel <jmoeckel@jastech-solutions.de>
2024-01-04 16:31:10 +0100

    Setup User Creation

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { DatabaseModule } from '@app/common/database/database.module';
+import { UserDocument, UserSchema } from './models/user.schema';
+import { LoggerModule } from '@app/common/logger/logger.module';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: UserDocument.name, schema: UserSchema },
+    ]),
+    LoggerModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
