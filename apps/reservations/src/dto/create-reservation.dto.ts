84c4caaf52a8fc6f03cdbc84401264d20f37fc51
jeanmarcmoeckel <jmoeckel@jastech-solutions.de>
2024-01-04 12:42:52 +0100

    Add validation decorators to dto

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,9 +1,20 @@
+import { IsDate, IsString, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
 
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
 
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
 
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
