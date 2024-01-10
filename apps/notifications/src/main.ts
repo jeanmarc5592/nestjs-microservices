055016108829d92c5d56112c1a512996ba9a8e7b
Jean-Marc <jmoeckel@jastech-solutions.de>
2024-01-10 08:25:09 +0100

    Scaffold notification workflow

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationsModule } from './notifications.module';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get<string>('PORT'),
+    },
+  });
+
+  app.useLogger(app.get(Logger));
+
+  app.startAllMicroservices();
 }
+
 bootstrap();
--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { LoggerModule } from '@app/common/logger/logger.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.string().required(),
+      }),
+    }),
+    LoggerModule,
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
