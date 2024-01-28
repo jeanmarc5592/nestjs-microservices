ecafdd2a37f5dfc3ad15beee7117f9b65a4d0e97
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-01-28 14:05:40 +0100

    Refactor Notifications Module to work with gRPC

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -3,16 +3,19 @@ import { NotificationsModule } from './notifications.module';
 import { Transport } from '@nestjs/microservices';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import { NOTIFICATIONS_PACKAGE_NAME } from '@app/common/types/notifications';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
   const configService = app.get(ConfigService);
 
   app.connectMicroservice({
-    transport: Transport.TCP,
+    transport: Transport.GRPC,
     options: {
-      host: '0.0.0.0',
-      port: configService.get<string>('PORT'),
+      package: NOTIFICATIONS_PACKAGE_NAME,
+      protoPath: join(__dirname, '../../../proto/notifications.proto'),
+      url: configService.getOrThrow('NOTIFICATIONS_GRPC_URL'),
     },
   });
 
