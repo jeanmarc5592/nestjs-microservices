4ac65d4f22ebfe13df17b027792f40e08f4f9701
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-01-28 13:04:33 +0100

    Create ts files based on proto files

---
+++ libs/common/src/types/auth.ts
@@ -0,0 +1,43 @@
+/* eslint-disable */
+import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
+import { Observable } from "rxjs";
+
+export const protobufPackage = "auth";
+
+export interface Authenticaion {
+  Authenticaion: string;
+}
+
+export interface UserMessage {
+  id: string;
+  email: string;
+  password: string;
+  roles: string[];
+}
+
+export const AUTH_PACKAGE_NAME = "auth";
+
+export interface AuthServiceClient {
+  login(request: Authenticaion): Observable<UserMessage>;
+}
+
+export interface AuthServiceController {
+  login(request: Authenticaion): Promise<UserMessage> | Observable<UserMessage> | UserMessage;
+}
+
+export function AuthServiceControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ["login"];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("AuthService", method)(constructor.prototype[method], method, descriptor);
+    }
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("AuthService", method)(constructor.prototype[method], method, descriptor);
+    }
+  };
+}
+
+export const AUTH_SERVICE_NAME = "AuthService";
